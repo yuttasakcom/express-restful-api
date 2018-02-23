ef7021c0e029693e638d1aa4e9342021b50136e4
YoProgrammer <yuttasakcom@gmail.com>
2018-02-24 01:08:12 +0700

    create controllers

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,23 +1,11 @@
 import { Router } from 'express'
+import UsersController from '@/controllers/UsersController'
 
 const router = Router()
 
-// Mock Users
-const users = [{ name: 'Alexandre' }, { name: 'Pooya' }, { name: 'Sébastien' }]
+const users = (req, res) => new UsersController(req, res)
 
-/* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.json(users)
-})
-
-/* GET user by ID. */
-router.get('/users/:id', function(req, res, next) {
-  const id = parseInt(req.params.id)
-  if (id >= 0 && id < users.length) {
-    res.json(users[id])
-  } else {
-    res.sendStatus(404)
-  }
-})
+router.get('/users', (req, res) => users(req, res).get())
+router.get('/users/:id', (req, res) => users(req, res).getById())
 
 export default router
